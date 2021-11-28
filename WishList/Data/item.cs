65e7902c6271ae2b069de71cf05fe30096fcd266
Syed Khalid <khalidshujat@gmail.com>
2021-11-28 03:58:48 +0000

    3.2

--- WishList/Data/item.cs
+++ WishList/Data/item.cs
@@ -1,6 +1,13 @@
-﻿namespace WishList.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Data
 {
     public class item
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
     }
 }
