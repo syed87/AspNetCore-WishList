191b6fff4cd89d2322cdfeedf62a3e9e50b5fbc6
Syed Khalid <khalidshujat@gmail.com>
2021-11-28 03:12:20 +0000

    2

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(Index);
+        }
+
+        public ActionResult Error()
+        {
+            return View(Error);
+        }
+    }
+}
