701d3be54e46fb6289f2973f29da7e2c95723634
Syed Khalid <khalidshujat@gmail.com>
2021-11-28 04:23:34 +0000

    3.4

--- WishList/Data/item.cs
+++ WishList/Data/item.cs
@@ -2,10 +2,9 @@
 
 namespace WishList.Models
 {
-    public class item
+    public class Item
     {
         public int Id { get; set; }
-
         [Required]
         [MaxLength(50)]
         public string Description { get; set; }
